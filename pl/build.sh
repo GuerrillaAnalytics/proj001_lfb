1c9e5cb3ddacbb00772d46b41ab77221b94bf30b
Enda Ridge <guerrillaanalytics@gmail.com>
2018-03-11 09:10:19 +0000

    created a build script to run all code

--- pl/build.sh
+++ pl/build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied. You must supply a database host"
+    exit 1
+fi
+
+
 HOST=$1
 
 # Find all files for the pipeline, ignoring folders and files we do not want to execute
@@ -7,6 +14,29 @@ find .  ! -name '*.md' ! -name 'build.sh' ! -name '*LICENSE' ! -path '*.git*'  !
 while read filename
 do
     echo ""
+    echo "-----------------------------------------------------------------------------"
     echo "Processing: $filename"    # ... or any other command using $filename
-    psql -h $HOST -d proj001_lfb -U postgres -w -f "$filename"
+    
+    # Extract the extension so we can decide what to do
+    filename_only=$(basename "${filename}")
+    fileext=${filename_only##*.}
+    echo "Extension: ${fileext}"
+    
+    # if the file is sql, then use psql tool
+    if [ "${fileext}" = "sql" ]
+    then
+        psql -h $HOST -d proj001_lfb -U postgres -w -f "$filename"
+    
+    # if the file is a sheel script then use bash
+    elif [ "${fileext}" = "sh" ]
+    then
+    
+        bash "$filename"
+    else
+        echo "Skipping file with extension '${fileext}' "
+    
+    fi
+    
 done
+
+echo "Finished build script"
