468c527343296b967ef191ba082d850f5d0fde2e
guerrillaanalytics <guerrillaanalytics@gmail.com>
2018-03-13 16:25:58 +0000

    created pointers to new data

---
+++ pl/03_l1/020_L1_LFB Incident data from January 2013 to December 2016.sql
@@ -0,0 +1,12 @@
+﻿
+/*Point to the latest version of the file*/
+drop view if exists proj001_lfb_1_0_0.L1_LFB_Incident_data_from_January_2013_to_December_2016;
+
+create view proj001_lfb_1_0_0.L1_LFB_Incident_data_from_January_2013_to_December_2016 as
+select * 
+from inputs.inp_002_02_LFB_Incident_data_from_January_2013_to_December_2016;
+
+/*
+select * from proj001_lfb_1_0_0.L1_LFB_Incident_data_from_January_2013_to_December_2016 limit 10;
+
+*/
---
+++ pl/03_l1/030_L1_LFB Incident data from January 2017.sql
@@ -0,0 +1,12 @@
+﻿
+/*Point to the latest version of the file*/
+drop view if exists proj001_lfb_1_0_0.L1_LFB_Incident_data_from_January_2017;
+
+create view proj001_lfb_1_0_0.L1_LFB_Incident_data_from_January_2017 as
+select * 
+from inputs.inp_002_03_LFB_Incident_data_from_January_2017;
+
+/*
+select * from proj001_lfb_1_0_0.L1_LFB_Incident_data_from_January_2017 limit 10;
+
+*/
